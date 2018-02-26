c11c27e98995232434ae090c51e0f09297c48c73
Cindy <jl7e12@gmail.com>
2018-02-26 14:57:43 +0000

    Added card limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
- attr_reader :balance
+  DEFAULT_LIMIT = 90
+  attr_reader :balance
 
-  def initialize
+  def initialize(limit = DEFAULT_LIMIT)
     @balance = 0
+    @limit = limit
   end
 
   def top_up(amount)
+    raise "Sorry, you've reached the limit of £#{@limit}" if @balance + amount > @limit
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,19 @@
 require 'oystercard'
   describe Oystercard do
+    oystercard = Oystercard.new
     it 'should have a balance of 0 when initialized' do
       expect(Oystercard.new.balance).to eq 0
     end
 
     it 'should respond to #top_up by adding money to the balance' do
-      oystercard = Oystercard.new
+
       expect { oystercard.top_up(10) }.to change {oystercard.balance}.by(10)
       expect { oystercard.top_up(10) }.to change {oystercard.balance}.from(10).to(20)
+    end
+
+    it 'should raise an error when exceed the limit' do
+
+      expect { oystercard.top_up(100) }.to raise_error("Sorry, you've reached the limit of £#{Oystercard::DEFAULT_LIMIT}")
     end
 
   end
