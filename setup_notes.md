44eb23a0292847069436f999dae8921e478534c1
julesnuggy <julesnuggy@gmail.com>
2018-02-26 14:40:27 +0000

    added top_up method and test

---
+++ setup_notes.md
@@ -0,0 +1,25 @@
+Classes
+* Card
+* Journey
+* Map
+
+Methods
+* Card: add_money
+* Card: pay
+* Card: touch_in
+* Card: touch_out
+* Card: check_history
+* ??? charge_penalty
+* ??? calc_fare
+
+Attributes
+* Card: limit
+* Card: balance
+* Card: ID
+* Journey: fare
+* Journey: touch_status
+* Journey: minimum_fare
+* Journey: start_loc
+* Journey: end_loc
+* Journey: history
+* Map: zone
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,13 @@
 require 'oystercard'
   describe Oystercard do
     it 'should have a balance of 0 when initialized' do
-
       expect(Oystercard.new.balance).to eq 0
+    end
+
+    it 'should respond to #top_up by adding money to the balance' do
+      oystercard = Oystercard.new
+      expect { oystercard.top_up(10) }.to change {oystercard.balance}.by(10)
+      expect { oystercard.top_up(10) }.to change {oystercard.balance}.from(10).to(20)
     end
 
   end
