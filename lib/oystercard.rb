4ca95022449452df7f94b5a429e884de7548e325
Cindy <jl7e12@gmail.com>
2018-02-26 15:38:21 +0000

    Created touch_in, touch_out, in_journey? methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
   DEFAULT_LIMIT = 90
-  attr_reader :balance
+  attr_reader :balance, :card_status
 
   def initialize(limit = DEFAULT_LIMIT)
     @balance = 0
     @limit = limit
+    @card_status = :not_in_use
   end
 
   def top_up(amount)
@@ -17,4 +18,17 @@ class Oystercard
     journey.fare = fare_amount
     @balance -= journey.fare
   end
+
+  def touch_in
+    @card_status = :in_use if @card_status == :not_in_use
+  end
+
+  def touch_out
+    @card_status = :not_in_use if @card_status == :in_use
+  end
+
+  def in_journey?
+    @card_status == :in_use
+  end
+  
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,22 @@ require 'oystercard'
       expect { oystercard.top_up(100) }.to raise_error("Sorry, you've reached the limit of £#{Oystercard::DEFAULT_LIMIT}")
     end
 
+    it "should start the journey when touch in" do
+      expect { oystercard.touch_in }.to change {oystercard.card_status}.from(:not_in_use).to(:in_use)
+    end
+
+    it 'should end the journey when touch out' do
+      expect { oystercard.touch_out }.to change {oystercard.card_status}.from(:in_use).to(:not_in_use)
+    end
+
+    it 'should be true when card_status is in_use' do
+      oystercard.touch_in
+      expect(oystercard.in_journey?).to be_truthy
+    end
+
+    it 'should be false when card_status is not_in_use' do
+      oystercard.touch_out
+      expect(oystercard.in_journey?).to be_falsey
+    end
+
   end
